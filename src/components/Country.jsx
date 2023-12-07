8fc8511e2d27ac78a2b48739e1a719517bdf6e6f
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-07 13:16:51 +0600

    country api fetch updated

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 const Country = (props) => {
-  const { name, population, region, flag } = props.country;
-  console.log(flag);
-  const flagStyle = {
-    height: "100px",
-    width: "150px",
-  };
+  const { name, population, region, flag, flags } = props.country;
+  console.log(props.country);
   return (
     <>
       <div style={{ border: "1px solid yellow", marginBottom: "1rem" }}>
         <h4 style={{ margin: "0", padding: "20px 0px" }}>
           This is {name.common}
         </h4>
-        <div style={{ backgroundColor: "white" }}>
-          <img src={flag} alt="" style={{ height: "100px", width: "150px" }} />
+        <div>
+          <img
+            src={flags.png}
+            alt=""
+            style={{ height: "100px", width: "150px" }}
+          />
         </div>
         <p style={{ margin: "0", padding: "20px 0px" }}>
           population : {population}
